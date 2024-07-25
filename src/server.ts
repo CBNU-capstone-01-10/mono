9f93a3f4d0536fa455ad301fd67366921fe9b494
seungho-hub <kmc54320@gmail.com>
2024-07-25 15:51:05 +0900

    replace session store to redis

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import 'dotenv/config';
 
 import http from 'http';
 import app from './app';
+import redisClient from './database/clients/redis';
 
 const server = http.createServer(app);
 
 function serverListening() {
   console.log(`Server is listening on port ${process.env.PORT}`);
+  redisClient.connect();
 }
+
+server.on('close', () => {
+  redisClient.disconnect();
+});
 
 server.listen(process.env.PORT, serverListening);
 
