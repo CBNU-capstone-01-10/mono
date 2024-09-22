90f8490c0c97a76f17f7c59882a73d50d8037dbb
seungho-hub <kmc54320@gmail.com>
2024-09-22 18:16:28 +0900

    add create action validation

--- src/validation/action.validation.ts
+++ src/validation/action.validation.ts
@@ -3,8 +3,8 @@ import { ValidationSchema } from '../../@types/validator';
 
 const createAction: ValidationSchema = {
   body: joi.object().keys({
-    location_x: joi.number(),
-    location_y: joi.number(),
+    location_x: joi.number().required(),
+    location_y: joi.number().required(),
   }),
   file: joi.required(),
 };
