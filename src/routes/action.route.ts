8aa2457947d453c56733d8d1c5a539c910ecf246
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-09-22 22:50:24 +0900

    update : append extension in uploading file name

--- src/routes/action.route.ts
+++ src/routes/action.route.ts
@@ -4,9 +4,23 @@ import validate from '../middleware/validate';
 import actionValidation from '../validation/action.validation';
 import { createAction } from '../controller/action.controller';
 import { uploadPath } from '../config/path.config';
-
+import mime from 'mime';
 import multer from 'multer';
-const upload = multer({ dest: uploadPath.action.capture });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath.action.capture);
+  },
+  filename: function (req, file, cb) {
+    const extension = mime.extension(file.mimetype);
+
+    if (extension) {
+      cb(null, (req.session.userId as number) + Date.now() + '.' + extension);
+    }
+  },
+});
+
+const upload = multer({ storage });
 
 const router = Router();
 
